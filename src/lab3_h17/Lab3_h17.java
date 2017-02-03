104b6b2a0b7216fe22d422872c1808ee03bc9903
David <David@172.29.121.184>
2017-02-02 21:00:17 -0500

    init

--- src/lab3_h17/Lab3_h17.java
+++ src/lab3_h17/Lab3_h17.java
@@ -16,6 +16,7 @@ public class Lab3_h17 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("hello world2");
     }
     
 }
